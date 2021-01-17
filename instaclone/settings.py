9bc2be345135e9cce61b6f58d60b20357925abae
Tony <tr33947@gmail.com>
2021-01-17 19:13:24 +0300

    deploy to heroku

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -150,3 +150,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_REDIRECT_URL = 'index'
 
 LOGOUT_REDIRECT_URL = 'login'
+
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
